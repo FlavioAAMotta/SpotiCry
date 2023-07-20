ce3a20c5eeefa914bdb10a0e2d35a3d3820eef5f
FlavioAAMotta <flavio.motta@sga.pucminas.br>
2023-07-20 11:58:08 -0300

    Fix on data

--- src/Data/UserData.ts
+++ src/Data/UserData.ts
@@ -2,15 +2,22 @@
 import { IUserData } from "../model/InterfaceUserData";
 import User from "../model/User";
 import { QueryDocumentSnapshot } from "@google-cloud/firestore";
-
-// Usar isso dentro do terminal
-// export GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/service-account-file.json"
-// admin.initializeApp({
-//     credential: admin.credential.applicationDefault(),
-//     databaseURL: 'https://spoticry.firebaseio.com'
-// });
 const admin = require("firebase-admin");
-const serviceAccount = require("C:\\Users\\Pichau\\.ssh\\spoticry-e8e8d-firebase-adminsdk-84g1b-8d6759b387.json");
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -31,7 +38,6 @@ export default class UserData implements IUserData {
 
       let user: User | null = null;
       snapshot.forEach((doc: QueryDocumentSnapshot) => {
-        console.log(doc.id, "=>", doc.data());
         const userData = doc.data();
         user = new User(
           userData.id,
